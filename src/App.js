ed66354d9a1af5d650076b965078b012d3454eea
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-08-02 22:55:50 +0300

    2.08

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import JSX from "./1-JSX/JSX"
+import Props from "./2-Props/Props"
+import Events from "./3-Events/Events"
+import Conditionals from "./4-Conditionals/Conditionals";
+import Lists from "./5-Lists/Lists";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <JSX/> */}
+      {/* <Props/> */}
+      {/* <Events/> */}
+      {/* <Conditionals/> */}
+      <Lists/>
     </div>
   );
 }
